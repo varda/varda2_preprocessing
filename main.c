269c61265fb5489d3e841c4fe4e9e6082f5bbab0
Mark Santcroos <m.a.santcroos@lumc.nl>
2019-12-09 10:52:57 +0100

    Add allele_count column based on genotype.
    The length of the GT array is assumed to be the alternative allele count.
    Missing alelles are ignored.

--- main.c
+++ main.c
@@ -16,67 +16,92 @@ main(int argc, char* argv[])
 
     int const threshold = atoi(argv[1]);  // FIXME: unsafe
 
-    htsFile* const restrict fh = bcf_open("-", "r");
+    htsFile* const fh = bcf_open("-", "r");
     if (NULL == fh)
     {
         fprintf(stderr, "bcf_open() failed\n");
         return EXIT_FAILURE;
     } // if
 
-    bcf_hdr_t* const restrict hdr = bcf_hdr_read(fh);
+    bcf_hdr_t* const hdr = bcf_hdr_read(fh);
 
     if (1 != bcf_hdr_nsamples(hdr))
     {
         fprintf(stderr, "#samples = %d\n", bcf_hdr_nsamples(hdr));
-        bcf_hdr_destroy(hdr);
-        bcf_close(fh);
-        return EXIT_FAILURE;
+        goto error1;
     } // if
+
 
     int nseq = 0;
     char const** seqnames = bcf_hdr_seqnames(hdr, &nseq);
     if (NULL == seqnames)
     {
         fprintf(stderr, "bcf_hdr_seqnames() failed\n");
-        bcf_hdr_destroy(hdr);
-        bcf_close(fh);
-        return EXIT_FAILURE;
+        goto error1;
     } // if
 
-    bcf1_t* restrict rec = bcf_init();
+    bcf1_t* rec = bcf_init();
     if (NULL == rec)
     {
         fprintf(stderr, "bcf_init() failed\n");
-        bcf_hdr_destroy(hdr);
-        bcf_close(fh);
-        return EXIT_FAILURE;
+        goto error2;
     } // if
 
-    int ndp_arr = 0;
-    int* restrict dp = NULL;
+    int32_t *dp = NULL;
+    int32_t *gt = NULL;
 
     while (0 == bcf_read(fh, hdr, rec))
     {
-        int depth = 0;
-        if (1 == bcf_get_format_int32(hdr, rec, "DP", &dp, &ndp_arr))
+        int32_t depth = 0;
+        if (1 == bcf_get_format_int32(hdr, rec, "DP", &dp, &(int){0}))
         {
             depth = dp[0];
-        } // else
+        } // if
 
-        if (threshold <= depth)
+        int ngt_arr = 0;
+        int const ploidy = bcf_get_format_int32(hdr, rec, "GT", &gt, &ngt_arr);
+        if (0 > ploidy)
         {
-            fprintf(stdout, "%s\t%d\t%d\n", seqnames[rec->rid],
-                                            rec->pos,
-                                            rec->pos + rec->rlen);
+            fprintf(stderr, "bcf_get_genotypes failed\n");
+            goto error;
+        } // if
+
+        if (ploidy != ngt_arr)
+        {
+            fprintf(stderr, "ploidy != count\n");
+            goto error;
+        } // if
+
+        if (threshold <= (int)depth)
+        {
+            fprintf(stdout, "%s\t%d\t%d\t%d\n", seqnames[rec->rid],
+                                                rec->pos,
+                                                rec->pos + rec->rlen,
+                                                ploidy);
         } // if
     } // while
 
-    free(seqnames);
-    free(dp);
-
     bcf_destroy(rec);
+    free(dp);
+    free(gt);
+    free(seqnames);
     bcf_hdr_destroy(hdr);
     bcf_close(fh);
 
     return EXIT_SUCCESS;
+
+error:
+    bcf_destroy(rec);
+    free(dp);
+    free(gt);
+
+error2:
+    free(seqnames);
+
+error1:
+    bcf_hdr_destroy(hdr);
+    bcf_close(fh);
+
+    return EXIT_FAILURE;
+
 } // main
