797b3ff33e3a677a0ce41b594a2a8f6a7faf13f3
Mark Santcroos <m.a.santcroos@lumc.nl>
2020-02-27 22:53:53 +0100

    Port merge code from python.

--- c/src/main.c
+++ c/src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>      // fprintf
 #include <stdlib.h>     // EXIT_*, atoi
 #include <getopt.h>     // getopt
+#include <sys/param.h>  // MIN, MAX
 
 #include "htslib/vcf.h"     // hts*, bcf_*
 
@@ -17,34 +18,38 @@ main(int argc, char* argv[])
 
     static char usage[] = "usage: %s [-t threshold] [-d distance] [-n]\n";
 
-    while ((c = getopt(argc, argv, "t:nd:")) != -1) {
-        switch (c) {
-        case 't':
-            threshold = atoi(optarg);
-            break;
-        case 'n':
-            nflag = 1;
-            merge = 0;
-            break;
-        case 'd':
-            dflag = 1;
-            distance = atoi(optarg);
-            break;
-        case '?':
-            err = 1;
-            break;
-        }
-    }
+    while ((c = getopt(argc, argv, "t:nd:")) != -1)
+    {
+        switch (c)
+        {
+            case 't':
+                threshold = atoi(optarg);
+                break;
+            case 'n':
+                nflag = 1;
+                merge = 0;
+                break;
+            case 'd':
+                dflag = 1;
+                distance = atoi(optarg);
+                break;
+            case '?':
+                err = 1;
+                break;
+        } // switch
+    } // while
 
-    if (nflag && dflag) {
+    if (nflag && dflag)
+    {
         fprintf(stderr, "Both no merge and distance specified!\n");
         err += 1;
-    }
+    } // if
 
-    if (err) {
+    if (err)
+    {
         fprintf(stderr, usage, argv[0]);
         return EXIT_FAILURE;
-    }
+    } // if
 
     htsFile* const fh = bcf_open("-", "r");
     if (NULL == fh)
@@ -60,7 +65,6 @@ main(int argc, char* argv[])
         fprintf(stderr, "#samples = %d\n", bcf_hdr_nsamples(hdr));
         goto error1;
     } // if
-
 
     int nseq = 0;
     char const** seqnames = bcf_hdr_seqnames(hdr, &nseq);
@@ -80,12 +84,30 @@ main(int argc, char* argv[])
     int32_t *dp = NULL;
     int32_t *gt = NULL;
 
-    while (0 == bcf_read(fh, hdr, rec))
-    {
+    int first = 1;
+    int jump = 1;
+
+    int window_start = 0;
+    int window_end = 0;
+    const char * window_chrom = "";
+    int window_ploidy = 0;
+
+    while (0 == bcf_read(fh, hdr, rec)) {
+
+        jump = 0;
+
         int32_t depth = 0;
         if (1 == bcf_get_format_int32(hdr, rec, "DP", &dp, &(int){0}))
         {
             depth = dp[0];
+        } // if
+
+        //
+        // If depth is below the threshold, no need to proceed
+        //
+        if (depth < threshold)
+        {
+            continue;
         } // if
 
         int ngt_arr = 0;
@@ -102,14 +124,76 @@ main(int argc, char* argv[])
             goto error;
         } // if
 
-        if (threshold <= (int)depth)
+        //
+        // Convenience handles
+        //
+        const char *chrom = seqnames[rec->rid];
+        int start = rec->pos;
+        int end = rec->pos + rec->rlen;
+
+        //
+        // When we don't merge, just print here and proceed
+        //
+        if (!merge)
         {
-            fprintf(stdout, "%s\t%ld\t%ld\t%d\n", seqnames[rec->rid],
-                                                rec->pos,
-                                                rec->pos + rec->rlen,
-                                                ploidy);
+            fprintf(stdout, "%s\t%d\t%d\t%d\n", chrom, start, end, ploidy);
+            continue;
         } // if
+
+        //
+        // We just started
+        //
+        if (first)
+        {
+            window_start = start;
+            window_end = end;
+            window_chrom = chrom;
+            window_ploidy = ploidy;
+
+            // eprint(f"First! c:{window_chrom} s:{start}, w_s={window_start} e:{end} w_e={window_end}")
+
+            first = 0;
+            continue;
+        } // if
+
+        if (window_chrom != chrom)
+        {
+            // eprint(f"Chrom changed from {window_chrom} to {chrom}.")
+            jump = 1;
+        } else if (window_ploidy != ploidy)
+        {
+            // eprint(f"Ploidy changed from {window_ploidy} to {ploidy}")
+            jump = 1;
+        } else if (window_end + distance < start)
+        {
+            // eprint("Gap! (window_end:%d < start:%d)" % (window_end + distance, start))
+            jump = 1;
+        } // if
+
+        if (jump)
+        {
+            fprintf(stdout, "%s\t%d\t%d\t%d\n", window_chrom, window_start, window_end, window_ploidy);
+
+            window_start = start;
+            window_end = end;
+            window_chrom = chrom;
+            window_ploidy = ploidy;
+        } else
+        {
+            window_start = MIN(window_start, start);
+            window_end = MAX(window_end, end);
+            // eprint(f"No jump! s:{start}, w_s={window_start} e:{end} w_e={window_end}")
+        } // if
+
     } // while
+
+    //
+    // If the last iteration of the loop was not a jump, we still need to print
+    //
+    if (merge && !jump)
+    {
+        fprintf(stdout, "%s\t%d\t%d\t%d\n", window_chrom, window_start, window_end, window_ploidy);
+    }
 
     bcf_destroy(rec);
     free(dp);
