a64d9c2a57e157a39e11d706b4442311a076df25
Mark Santcroos <m.a.santcroos@lumc.nl>
2020-02-27 14:40:50 +0100

    Refactor and argparse.

--- python/gvcf2coverage.py
+++ python/gvcf2coverage.py
@@ -1,92 +1,107 @@
 import sys
+import argparse
 from cyvcf2 import VCF
 
-threshold = 10
-distance = 0
 
 def eprint(*args):
     print(*args, file=sys.stderr)
 
-vcf = VCF(fname='-', gts012=False, lazy=False, strict_gt=False)
 
-eprint(f"samples: {vcf.samples}")
-assert len(vcf.samples) == 1
+def main(threshold, distance):
 
-eprint(f"number of seqnames: {len(vcf.seqnames)}")
-assert len(vcf.seqnames) > 0
+    vcf = VCF(fname='-', gts012=False, lazy=False, strict_gt=False)
 
-first = True
+    # eprint(f"samples: {vcf.samples}")
+    assert len(vcf.samples) == 1
 
-for entry in vcf:
+    # eprint(f"number of seqnames: {len(vcf.seqnames)}")
+    assert len(vcf.seqnames) > 0
 
-    jump = False
-
-    # Depth
-    dp = entry.format('DP')
-
-    if dp is None:
-        depth = 0
-    else:
-        depth = dp[0][0]
+    first = True
 
     #
-    # If depth is below the threshold, no need to go proceed
+    # Loop over all entries
     #
-    if depth < threshold:
-        continue
+    for entry in vcf:
+
+        jump = False
+
+        # Depth
+        dp = entry.format('DP')
+
+        if dp is None:
+            depth = 0
+        else:
+            depth = dp[0][0]
+
+        #
+        # If depth is below the threshold, no need to go proceed
+        #
+        if depth < threshold:
+            continue
+
+        #
+        # Convenience handles
+        #
+        chrom = entry.CHROM
+        start = entry.start
+        end = entry.end
+        ploidy = entry.ploidy
+
+        #
+        # We just started
+        #
+        if first:
+            # First entry
+            window_start = start
+            window_end = end
+            window_chrom = chrom
+            window_ploidy = ploidy
+
+            first = False
+
+            # eprint(f"First! c:{window_chrom} s:{start}, w_s={window_start} e:{end} w_e={window_end}")
+
+            continue
+
+        if window_chrom != chrom:
+            # eprint(f"Chrom changed from {window_chrom} to {chrom}.")
+            jump = True
+
+        elif window_ploidy != ploidy:
+            # eprint(f"Ploidy changed from {window_ploidy} to {ploidy}")
+            jump = True
+
+        elif window_end + distance < start:
+            # eprint("Gap! (window_end:%d < start:%d)" % (window_end + distance, start))
+            jump = True
+
+        if jump:
+            # eprint("Jump!")
+            print(window_chrom, window_start, window_end, window_ploidy, sep="\t")
+
+            window_start = start
+            window_end = end
+            window_chrom = chrom
+            window_ploidy = ploidy
+
+        else:
+            window_start = min(window_start, start)
+            window_end = max(window_end, end)
+            # eprint(f"No jump! s:{start}, w_s={window_start} e:{end} w_e={window_end}")
 
     #
-    # Convenience handles
+    # If the last iteration of the loop was not a jump, we still need to print
     #
-    chrom = entry.CHROM
-    start = entry.start
-    end = entry.end
-    ploidy = entry.ploidy
-
-    #
-    # We just started
-    #
-    if first:
-        # First entry
-        window_start = start
-        window_end = end
-        window_chrom = chrom
-        window_ploidy = ploidy
-
-        first = False
-
-        eprint(f"First! c:{window_chrom} s:{start}, w_s={window_start} e:{end} w_e={window_end}")
-
-        continue
-
-    if window_chrom != chrom:
-        #eprint(f"Chrom changed from {window_chrom} to {chrom}.")
-        jump = True
-
-    elif window_ploidy != ploidy:
-        #eprint(f"Ploidy changed from {window_ploidy} to {ploidy}")
-        jump = True
-
-    elif window_end + distance < start:
-        #eprint("Gap! (window_end:%d < start:%d)" % (window_end + distance, start))
-        jump = True
-
-    if jump:
-        #eprint("Jump!")
+    if not jump:
         print(window_chrom, window_start, window_end, window_ploidy, sep="\t")
 
-        window_start = start
-        window_end = end
-        window_chrom = chrom
-        window_ploidy = ploidy
 
-    else:
-        window_start = min(window_start, start)
-        window_end = max(window_end, end)
-        #eprint(f"No jump! s:{start}, w_s={window_start} e:{end} w_e={window_end}")
+if __name__ == '__main__':
 
-#
-# If the last iteration of the loop was not a jump, we still need to print
-#
-if not jump:
-    print(window_chrom, window_start, window_end, window_ploidy, sep="\t")
+    parser = argparse.ArgumentParser(description='Convert gVCF to coverage.')
+    parser.add_argument('--threshold', '-t', type=int, default=10, help='DP threshold')
+    parser.add_argument('--distance', '-d',  type=int, default=0, help='Merging distance')
+    args = parser.parse_args()
+
+    main(args.threshold, args.distance)
