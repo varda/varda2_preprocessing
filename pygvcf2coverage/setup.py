062a2ac1f463022439930cbec89ee3874dca83bc
Mark Santcroos <m.a.santcroos@lumc.nl>
2020-05-01 09:24:32 +0200

    Rename and fixed entry point.

--- pygvcf2coverage/setup.py
+++ pygvcf2coverage/setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-setup(name='gvcf2coverage',
+setup(name='pygvcf2coverage',
     version='0.1',
     description='A python tool to extract coverage from gvcf files.',
     url='http://github.com/varda/varda2_preprocessing',
     author='Mark Santcroos',
     author_email='m.a.santcroos@lumc.nl',
     license='MIT',
-    packages=['gvcf2coverage'],
+    packages=['pygvcf2coverage'],
     zip_safe=False,
     entry_points = {
-        'console_scripts': ['gvcf2coverage=gvcf2coverage'],
+        'console_scripts': ['pygvcf2coverage=pygvcf2coverage:main'],
     },
     install_requires=[
         'cyvcf2'
